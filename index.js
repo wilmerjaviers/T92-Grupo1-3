50b49b6a5f2569d68fddb97bb59dd49eec1e0de1
WILSON ODIEL FLORES HERRERA <wilson_flores@unitec.edu>
2025-02-26 00:51:55 -0600

    se corrige la api para actualizar ya que el anterior sobrescribia el archivo json

--- index.js
+++ index.js
@@ -4,22 +4,23 @@ const path = require('path')
 const app = express();   
 const PORT = 3000;
 //Para obtener el directorio del archivo libros.json
-const pathLibros = path.join(__dirname, 'libros.json')
+const pathLibros = path.join(__dirname, './libros.json')
 //Middleware
 app.use(express.json());
 
 //funcion para leer el archivo
 const leerLibros = ()=>{
     const data = fs.readFileSync(pathLibros, 'utf-8');
-    return data
+    const dataJson = JSON.parse(data);
+    return dataJson
 };
 //funcion para actualizar
-const saveLibros =  (data) =>{
-    fs.writeFileSync(pathLibros, JSON.stringify(data), 'utf-8')
+const saveLibros =  (dataJson) =>{
+    fs.writeFileSync(pathLibros, JSON.stringify(dataJson), 'utf-8')
 }
 
 //cargando el arreglo
-const libros = JSON.parse(leerLibros());
+const libros = leerLibros();
 
 //Api para obtener datos
 app.get('/libros',(req,res)=>{
@@ -27,23 +28,18 @@ app.get('/libros',(req,res)=>{
 });
 
 // Api para actualizar registros
-app.put('/libros',(req,res)=>{
-    let reqLibros = req.body;
-    let exist = false;
-    libros.forEach(books => {
-        if(books.id === reqLibros.id){
-            exist=true;
-            books.titulo = reqLibros.titulo;
-            books.autor = reqLibros.autor;
-            books.anio = reqLibros.anio;
-            books.disponible = reqLibros.disponible;
-        }
-    });
-    if(exist){
-        res.json({status: 200, message: 'Success', reqLibros})
-        saveLibros(reqLibros)
+app.put('/libros/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    let datosNuevos = req.body;
+    let libroId = libros.findIndex(libro => libro.id === id);
+
+    if (libroId !== -1){
+        //Se actualiza el arreglo usando el operador de propagacion
+        libros[libroId]={...libros[libroId], ...datosNuevos}
+        saveLibros(libros)
+        res.status(200).json({status: 200, message: "Libro Actualizaod", libro: libros[libroId]})
     }else{
-        res.status(400).json({status: 400, message: 'Bad Request', reqLibros})
+        res.status(400).json({status:404, message: "Libro no encontrado"})
     }
 });
 
